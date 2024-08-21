1fb4c23a5ff655d6fc2b0b4a49efdd11f4b4843b
Arthur Iasbeck <arthuriasbeck@gmail.com>
2024-08-21 17:27:10 -0300

    Atualização.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MyPackage",
+    name="package-test",
     version="0.1",
     packages=find_packages(),
     description="A brief description of MyPackage.",
